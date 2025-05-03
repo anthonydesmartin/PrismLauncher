14d21e1feed3b180733e8581f3de4385d814928b
Trial97 <alexandru.tripon97@gmail.com>
2025-05-03 12:25:18 +0300

    feat: store provider dependencies
    Signed-off-by: Trial97 <alexandru.tripon97@gmail.com>

--- launcher/modplatform/ModIndex.h
+++ launcher/modplatform/ModIndex.h
@@ -57,6 +57,11 @@ QString toString(Side side);
 Side fromString(QString side);
 }  // namespace SideUtils
 
+namespace DependencyTypeUtils {
+QString toString(DependencyType type);
+DependencyType fromString(const QString& str);
+}  // namespace DependencyTypeUtils
+
 namespace ProviderCapabilities {
 const char* name(ResourceProvider);
 QString readableName(ResourceProvider);
