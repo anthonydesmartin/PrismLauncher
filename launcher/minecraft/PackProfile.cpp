PlaceholderMC/PlaceholderMC

add setting to update minecraft instance setting to the latest available version
Signed-off-by: Trial97 <alexandru.tripon97@gmail.com>

launcher/minecraft/PackProfile.cpp
@@ -1069,3 +1069,20 @@ QList<ModPlatform::ModLoaderType> PackProfile::getModLoadersList()
     }
     return result;
 }
+
+void PackProfile::updateLatesMinecraft()
+{
+    const QString uid = "net.minecraft";
+    auto patch = getComponent(uid);
+    patch->waitLoadMeta();  // make sure we have latest versions
+    auto list = patch->getVersionList();
+    if (!list) {
+        return;
+    }
+
+    auto latest = list->getLatest();
+
+    qDebug() << "Change" << uid << "to" << latest.get();
+    setComponentVersion(uid, latest->descriptor(), true);
+    resolve(Net::Mode::Online);
+}

anthonydesmartin/PrismLauncher

launcher/minecraft/PackProfile.cpp
@@ -1061,3 +1061,20 @@ QList<ModPlatform::ModLoaderType> PackProfile::getModLoadersList()
     }
     return result;
 }
+
+void PackProfile::updateLatestMinecraft()
+{
+    const QString uid = "net.minecraft";
+    auto patch = getComponent(uid);
+    patch->waitLoadMeta();  // make sure we have latest versions
+    auto list = patch->getVersionList();
+    if (!list) {
+        return;
+    }
+
+    auto latest = list->getLatest();
+
+    qDebug() << "Change" << uid << "to" << latest.get();
+    setComponentVersion(uid, latest->descriptor(), true);
+    resolve(Net::Mode::Online);
+}